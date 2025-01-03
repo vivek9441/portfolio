00ec2965d733d30ddc82dce045bedeae46a68ea7
Bjorn Melin <bjornmelin16@gmail.com>
2025-01-02 21:31:51 -0700

    feat(config): Update Next.js configuration for static export and improve image handling
    - Added 'output: export' to enable static file generation in Next.js 14+ during the build process.
    - Cleaned up image hostname configuration for better readability.
    - Ensured consistent formatting in environment variables for clarity.
    
    These changes enhance the build process and improve the overall configuration of the Next.js application.

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // 1) In Next.js 14+, this tells Next.js to produce static files in "out/" during `next build`.
+    output: 'export',
+
     images: {
         unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'bjornmelin.io',
+                hostname: 'bjornmelin.io'
             }
         ]
     },
     trailingSlash: true,
     reactStrictMode: true,
+
     env: {
         NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
-        NEXT_PUBLIC_CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL,
+        NEXT_PUBLIC_CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL
     }
 };
 
